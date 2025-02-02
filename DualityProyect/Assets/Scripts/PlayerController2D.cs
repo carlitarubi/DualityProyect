6e1caf1d8e5c905f5c322978f5bbd27772a4238a
carlitarubi <ccarabano99@gmail.com>
2025-02-02 12:33:01 +0100

    animaciones

--- DualityProyect/Assets/Scripts/PlayerController2D.cs
+++ DualityProyect/Assets/Scripts/PlayerController2D.cs
@@ -84,9 +84,9 @@ public class PlayerController2D : MonoBehaviour
     void HandleAnimations()
     {
         // Conector de valores generales con parametros de animación
-        playerAnim.SetBool("isJumping", !isGrounded);
-        if (moveInput.x > 0 || moveInput.x < 0) playerAnim.SetBool("isRunning", true);
-        else playerAnim.SetBool("isRunning", false);
+        playerAnim.SetBool("Jump", !isGrounded);
+        if (moveInput.x > 0 || moveInput.x < 0) playerAnim.SetBool("Run", true);
+        else playerAnim.SetBool("Run", false);
     }
 
     #region Input Events
