0e478989f579712f25d05f7315de7cff5670fa98
carlitarubi <ccarabano99@gmail.com>
2025-02-02 17:21:33 +0100

    camara CArlaMyC

--- DualityProyect/Assets/Scripts/SceneChanger.cs
+++ DualityProyect/Assets/Scripts/SceneChanger.cs
@@ -5,17 +5,11 @@ using UnityEngine.SceneManagement; //Libreria que permite usar metodos de carga/
 
 public class SceneChanger : MonoBehaviour
 {
-    public void SceneLoader(int sceneToLoad)
-    {
-        SceneManager.LoadScene(sceneToLoad);
-    }
+   
 
-    public void ExitGame()
-    {
-        Application.Quit(); //Salir de la aplicación, cierra el juego completamente
-    }
+    
 
-    [SerializeField] private string Escena;
+    public string Escena;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
